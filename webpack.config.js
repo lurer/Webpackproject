628c169c83eb5531d8b262e4490e2ecf6ebb5c83
Espen Zaal <espenbz@gmail.com>
2017-03-10 11:23:00 +0100

    Split webconfig into prod and dev config files. Will comment and optimize the config later..

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,6 @@
 var webpack = require('webpack');
 var path = require('path');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
 
 const SRC_DIR = path.resolve(__dirname, './content/src');
 const DIST_DIR = path.resolve(__dirname, './content/dist');
@@ -14,66 +12,15 @@ const VENDOR_LIBS = [
 
 const PROD = (process.env.NODE_ENV === 'production');
 
-
-
-/////////////////////// Plugins for Javascript ///////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////
-getJSPlugins = () =>{
-  let plugins = [];
-
-  plugins.push(
-    //will find duplicate depdendencies and add to vendor bundle
-    new webpack.optimize.CommonsChunkPlugin({ 
-      names: ['vendor', 'manifest']
-    }),
-    //Will automatically add all script files generated into an index.html in dist folder
-    new HtmlWebpackPlugin({
-      template: SRC_DIR +'/index.html' //file used as template
-    }),
-    //Looks for NODE_ENV variable in package.json scripts object.
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-    })
-  )
-  if(PROD){
-    plugins.push(
-        new webpack.optimize.UglifyJsPlugin({
-        compress: {warnings: false},
-        minimize:true,
-        sourceMap: true
-      })
-    )
-  }
-  return plugins;
-}
-
-
-///////////////////////// Plugins for CSS ////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////
-getCSSPlugins = () =>{
-  plugins = [];
-      plugins.push(new ExtractTextPlugin('[name].css'))
-      if(PROD){
-        plugins.push(new OptimizeCssAssetsPlugin())
-      }
-      return plugins;
-}
-
-
-/////////////////////////// Webpack config ///////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////
-module.exports = [{
+module.exports = {
   
   ///////////////////////// Javascript part /////////////////////////////////////////
   entry: {
-    bundle: SRC_DIR +'/js/app.jsx', //Our code
     vendor: VENDOR_LIBS //vendor code (dependencies)
   },
-  name: 'js',
-  devtool: 'source-map',
+  devtool: PROD ? 'source-map': 'eval',
   output: {
     path: DIST_DIR,
-    filename: PROD ?'[name].[chunkhash].min.js' : '[name].[chunkhash].js' //will make one file per entry point, using its name
   },
   resolve:{
     extensions:['.js', '.jsx'] //remove the need to import using .jsx extensions
@@ -95,28 +42,10 @@ module.exports = [{
       }
     ]
   },
-  plugins: getJSPlugins()
-},
-
-/////////////////////////////// CSS part /////////////////////////////////////////
-{
-  entry: SRC_DIR + '/sass/main.scss',
-  name: 'css',
-  output:{
-    path: DIST_DIR,
-    filename: '[name].css'
-  },
-  module:{
-    rules:[
-      {
-        //use:['style', 'css?sourceMap', 'sass?sourceMap'],
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use:'css-loader?sourceMap!sass-loader?sourceMap'
-        }),
-        test: /\.scss$/
-      }
-    ]
-  },
-  plugins: getCSSPlugins()
-}];
+  plugins: [
+    //Looks for NODE_ENV variable in package.json scripts object.
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    })
+  ]
+}
---
+++ webpack.prod.js
@@ -0,0 +1,39 @@
+var webpack = require('webpack');
+var config = require('./webpack.config');
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+const SRC_DIR = path.resolve(__dirname, './content/src');
+const DIST_DIR = path.resolve(__dirname, './content/dist');
+
+config.entry.bundle = [SRC_DIR +'/js/app.jsx', SRC_DIR + '/sass/main.scss'] //Our code
+config.output.filename = '[name].[chunkhash].js' //will make one file per entry point, using its name
+
+config.module.rules.push(
+    {
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use:'css-loader?sourceMap!sass-loader?sourceMap'
+        }),
+        test: /\.scss$/
+    }
+)
+
+config.plugins.push(
+    //will find duplicate depdendencies and add to vendor bundle
+    new webpack.optimize.CommonsChunkPlugin({ 
+      names: ['vendor', 'manifest']
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+        compress: {warnings: false},
+        minimize:true,
+        sourceMap: true
+    }),
+    new ExtractTextPlugin('[name].css'),
+    new OptimizeCssAssetsPlugin()
+)
+
+
+module.exports = config;
