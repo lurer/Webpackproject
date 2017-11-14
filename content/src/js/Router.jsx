da690e7fea1b22950d8eaff357b886a69d1f15cd
Espen Zaal <espenbz@gmail.com>
2017-11-14 19:22:36 +0100

    Implemented a simple configuration for React Router v4 using BrowserRouter. Added async import of routes on the fly using an azyncComponent and use of code splitting

--- content/src/js/Router.jsx
+++ content/src/js/Router.jsx
@@ -1,24 +1,44 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-import { hashHistory } from 'react-router';
 
 import Header from './components/Header';
 import Home from './components/Home';
-import Article from './components/Article';
-import Product from './components/Product';
 
+
+const asyncComponent = getComponent =>
+  class AsyncComponent extends React.Component {
+    static Component = null
+    state = { Component: AsyncComponent.Component };
+
+    componentWillMount = () => {
+      if (!this.state.Component) {
+        getComponent().then(({ default: Component }) => {
+          AsyncComponent.Component = Component;
+          this.setState({ Component });
+        });
+      }
+    }
+
+    render = () => {
+      const { Component } = this.state;
+      if (Component) {
+        return <Component {...this.props} />;
+      }
+      return null;
+    }
+  };
 
 const PrimaryLayout = () => (
 
   <div className="container primary-layout">
     <header>
-      <Header />  
+      <Header />
     </header>
-    
+
     <main>
-      <Route path="/" exact component={Home}/>
-      <Route path="/article" component={Article}/>
-      <Route path="/product" component={Product}/>
+      <Route path="/" exact component={Home} />
+      <Route path="/article" component={asyncComponent(() => import('./components/Article'))} />
+      <Route path="/product" component={asyncComponent(() => import('./components/Product'))} />
     </main>
   </div>
 );
@@ -27,7 +47,7 @@ const PrimaryLayout = () => (
 const Routes = () => (
 
   <BrowserRouter>
-    <PrimaryLayout />    
+    <PrimaryLayout />
   </BrowserRouter>
-)
+);
 export default Routes;
