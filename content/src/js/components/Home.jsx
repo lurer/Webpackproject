97e89bf0050ad7f8cf07997279e748a6c57fd856
Espen Zaal <espenbz@gmail.com>
2017-11-14 19:14:38 +0100

    Cleaned up according to eslint

--- content/src/js/components/Home.jsx
+++ content/src/js/components/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { observer } from 'mobx-react';
 
-const Home = observer(({ children }) =>
+const Home = observer(({ children }) => (
   <div>
     <h1>Home</h1>
     {children}
   </div>
-);
+));
 
 export default Home;
