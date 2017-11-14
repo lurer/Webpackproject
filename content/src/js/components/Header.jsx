2b1ecf9bfc1ed5bd169671531129ac57620d4ccb
Espen Zaal <espenbz@gmail.com>
2017-11-14 19:14:15 +0100

    Cleaned up according to eslint

--- content/src/js/components/Header.jsx
+++ content/src/js/components/Header.jsx
@@ -1,27 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 
-class Header extends Component {
+const Header = () => (
 
-  constructor(props) {
-    super(props);
-  }
-
-
-  render() {
-    return (
-      <div className="row">
-        <nav>
-          <ul className="mainmenu">
-            <li className="mainmenu--element"><Link to="/">Home</Link></li>
-            <li className="mainmenu--element"><Link to="/product">Articles</Link></li>
-            <li className="mainmenu--element"><Link to="/article">Products</Link></li>
-          </ul>
-        </nav>
-      </div>
-    );
-  }
-}
+  <div className="row">
+    <nav>
+      <ul className="mainmenu">
+        <li className="mainmenu--element"><Link to="/">Home</Link></li>
+        <li className="mainmenu--element"><Link to="/article">Articles</Link></li>
+        <li className="mainmenu--element"><Link to="/product">Products</Link></li>
+      </ul>
+    </nav>
+  </div>
+);
 
 export default Header;
